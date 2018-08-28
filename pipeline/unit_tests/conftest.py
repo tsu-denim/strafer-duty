1690327dcfcb237da059f46f11ee37d58a0d19ac
Kurt Waechter <kurt.waechter@blackboard.com>
2018-08-28 18:57:15 -0400

    Restoring old test command

--- pipeline/unit_tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Make sure that the application source directory (this directory's parent) is
-# on sys.path.
-
-here = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, here)
-print(sys.path)
