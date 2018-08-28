d1cdf64e41b3f4578eaf2ea8a491562d5b511f0e
Kurt Waechter <kurt.waechter@blackboard.com>
2018-08-28 02:01:42 -0400

    Switching back to shell script

--- pipeline/stages/build_install_local_library.sh
+++ pipeline/stages/build_install_local_library.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-export PYTHONPATH=${PYTHONPATH}:$(pwd)/dist/functions
-
-pip install -r requirements.txt
 pip install ./
--- run_pipeline_stages.sh
+++ run_pipeline_stages.sh
@@ -20,44 +20,44 @@ echo "##################################################"
 echo "##################################################"
 echo "###### STAGE - CLEAN_STALE_DISTRIBUTION ##########"
 echo "##################################################"
-./pipeline/stages/clean_stale_distribution.sh
+#./pipeline/stages/clean_stale_distribution.sh
 
 # Build static linked distributable and install
 # TODO: Add log groups to yaml template with retention policy so they are managed as part of the stack
 echo "##################################################"
 echo "###### STAGE - BUILD_CF_DISTRIBUTION #############"
 echo "##################################################"
-./pipeline/stages/build_cf_distribution.sh
+#./pipeline/stages/build_cf_distribution.sh
 
 # Build CloudFormation stack template and upload to S3 Bucket
 echo "##################################################"
 echo "###### STAGE - ZIP_CF_DISTRIBUTION ###############"
 echo "##################################################"
-./pipeline/stages/zip_cf_distributable.sh
+#./pipeline/stages/zip_cf_distributable.sh
 
 # Deploy CloudFormation stack
 echo "##################################################"
 echo "###### STAGE - DEPLOY_CF_STACK ###################"
 echo "##################################################"
-python3 -u ./pipeline/stages/deploy.py
+#python3 -u ./pipeline/stages/deploy.py
 
 # TODO: Pipe runTest logs to s3 and download for integration tests
 # Run protractor-sync integration smoke test, set to max 1 in config
 echo "##################################################"
 echo "###### STAGE - RUN_INTEGRATION_PROTRACTOR-SYNC ###"
 echo "##################################################"
-./pipeline/stages/run_integration_protractor-sync.sh
+#./pipeline/stages/run_integration_protractor-sync.sh
 
 # Run junit jasmine integration tests
 echo "##################################################"
 echo "###### STAGE - RUN_INTEGRATION_JASMINE-JUNIT #####"
 echo "##################################################"
-./pipeline/stages/run_integration_jasmine-junit.sh
+#./pipeline/stages/run_integration_jasmine-junit.sh
 
 # Run allure jasmine integration tests
 echo "##################################################"
 echo "###### STAGE - RUN_INTEGRATION_JASMINE-ALLURE ####"
 echo "##################################################"
-cp categories.json allure-results
-allure generate allure-results
-./pipeline/stages/run_integration_jasmine-allure.sh
+#cp categories.json allure-results
+#allure generate allure-results
+#./pipeline/stages/run_integration_jasmine-allure.sh
