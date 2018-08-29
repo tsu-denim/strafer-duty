9b874392d4d907542e2f32ea73250771a09252b5
Kurt Waechter <kurt.waechter@blackboard.com>
2018-08-28 23:39:37 -0400

    Added build badge, appended '&' to relevant build stages to run in parallel.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# strafer-duty
+# strafer-duty [![Build Status](https://travis-ci.org/tsu-denim/strafer-duty.svg?branch=master)](https://travis-ci.org/tsu-denim/strafer-duty)
 Massively parallel processing with AWS Lambda, Function as a Service (FaaS).
--- pipeline/stages/build_cf_distribution.sh
+++ pipeline/stages/build_cf_distribution.sh
@@ -6,10 +6,10 @@ echo $CHROME_VERSION
 
 sudo -H pip3 install . -t dist
 
+wget https://github.com/tsu-denim/xvfb-portable/releases/download/v1.0/xvfb.tar.gz &
+wget https://github.com/tsu-denim/node-portable/releases/download/node-v6.11.4-linux-x64/node.tar.gz &
+wget https://github.com/tsu-denim/ffmpeg-portable/releases/download/v0.1-alpha/ffmpeg.tar.gz &
 wget https://github.com/tsu-denim/chromium-full-gtk2-lambda/releases/download/63.2.33/chrome-63.0.3239.84.tar.gz
-wget https://github.com/tsu-denim/xvfb-portable/releases/download/v1.0/xvfb.tar.gz
-wget https://github.com/tsu-denim/node-portable/releases/download/node-v6.11.4-linux-x64/node.tar.gz
-wget https://github.com/tsu-denim/ffmpeg-portable/releases/download/v0.1-alpha/ffmpeg.tar.gz
 
 # Untar ffmpeg and node runtime environment into lambda package
 sudo tar -xf ./ffmpeg.tar.gz -C ./dist/
