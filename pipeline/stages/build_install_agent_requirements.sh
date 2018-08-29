01974c09641646e4fa702a9823cad1760a34565a
Kurt Waechter <kurt.waechter@blackboard.com>
2018-08-29 02:25:05 -0400

    Moving vital agent install commands back to travis

--- pipeline/stages/build_install_agent_requirements.sh
+++ pipeline/stages/build_install_agent_requirements.sh
@@ -6,11 +6,4 @@ wget https://github.com/tsu-denim/node-portable/releases/download/node-v6.11.4-l
 wget https://github.com/tsu-denim/ffmpeg-portable/releases/download/v0.1-alpha/ffmpeg.tar.gz --quiet &
 wget https://github.com/tsu-denim/chromium-full-gtk2-lambda/releases/download/63.2.33/chrome-63.0.3239.84.tar.gz --quiet &
 
-sudo -H apt-get install software-properties-common -qq
-sudo -H add-apt-repository --yes ppa:deadsnakes/ppa -qq
-sudo -H apt-get update -qq
-sudo -H apt-get install python3.6 -qq
-curl -sS https://bootstrap.pypa.io/get-pip.py | sudo -H python3.6
-sudo -H pip3 install awscli --upgrade --user --quiet
-
 wait
