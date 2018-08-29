5bff89587a45ae7c5a08d6afabe47be78a7f06c6
Kurt Waechter <kurt.waechter@blackboard.com>
2018-08-29 01:45:57 -0400

    Attempting CF deploy

--- pipeline/stages/build_install_local_library.sh
+++ pipeline/stages/build_install_local_library.sh
@@ -3,6 +3,7 @@ set -x
 
 sudo -H pip3 install -r requirements.txt &
 sudo -H pip3 install ./ &
+pip3 install awscli --upgrade --user
 
 # Wait until python library has been installed
 wait
--- run_pipeline_stages.sh
+++ run_pipeline_stages.sh
@@ -9,38 +9,39 @@ echo $PYTHONPATH
 echo "##################################################"
 echo "###### STAGE - DOWNLOAD_INSTALL_REQUIREMENTS #####"
 echo "##################################################"
-./pipeline/stages/build_install_agent_requirements.sh # Wait
+./pipeline/stages/build_install_agent_requirements.sh
 
- # Run unit tests
-(echo "##################################################";
- echo "###### STAGE - RUN_UNIT_TESTS ####################";
- echo "##################################################";
- ./pipeline/stages/run_unit_tests.sh &
+# Run unit tests
+echo "##################################################";
+echo "###### STAGE - RUN_UNIT_TESTS ####################";
+echo "##################################################";
+./pipeline/stages/run_unit_tests.sh
 
- # Cleanup any stale distribution packages
- echo "##################################################";
- echo "###### STAGE - CLEAN_STALE_DISTRIBUTION ##########";
- echo "##################################################";
- ./pipeline/stages/clean_stale_distribution.sh;
 
- # Install library
- echo "##################################################";
- echo "###### STAGE - BUILD_INSTALL_PYTHON_LIBRARY ######";
- echo "##################################################";
- ./pipeline/stages/build_install_local_library.sh &
+# Cleanup any stale distribution packages
+echo "##################################################"
+echo "###### STAGE - CLEAN_STALE_DISTRIBUTION ##########"
+echo "##################################################"
+./pipeline/stages/clean_stale_distribution.sh
 
- # Build static linked distributable and install
- # TODO: Add log groups to yaml template with retention policy so they are managed as part of the stack
- echo "##################################################";
- echo "###### STAGE - BUILD_CF_DISTRIBUTION #############";
- echo "##################################################";
- ./pipeline/stages/build_cf_distribution.sh;
+# Install library
+echo "##################################################"
+echo "###### STAGE - BUILD_INSTALL_PYTHON_LIBRARY ######"
+echo "##################################################"
+./pipeline/stages/build_install_local_library.sh &
 
- # Build CloudFormation stack template and upload to S3 Bucket
- echo "##################################################";
- echo "###### STAGE - ZIP_CF_DISTRIBUTION ###############";
- echo "##################################################";
- ./pipeline/stages/zip_cf_distributable.sh) &
+# Build static linked distributable and install
+# TODO: Add log groups to yaml template with retention policy so they are managed as part of the stack
+echo "##################################################"
+echo "###### STAGE - BUILD_CF_DISTRIBUTION #############"
+echo "##################################################"
+./pipeline/stages/build_cf_distribution.sh
+
+# Build CloudFormation stack template and upload to S3 Bucket
+echo "##################################################"
+echo "###### STAGE - ZIP_CF_DISTRIBUTION ###############"
+echo "##################################################"
+./pipeline/stages/zip_cf_distributable.sh
 
 wait
 
@@ -48,7 +49,7 @@ wait
 echo "##################################################"
 echo "###### STAGE - DEPLOY_CF_STACK ###################"
 echo "##################################################"
-#python3 -u ./pipeline/stages/deploy.py
+python3.6 -u ./pipeline/stages/deploy.py
 
 # TODO: Pipe runTest logs to s3 and download for integration tests
 # Run protractor-sync integration smoke test, set to max 1 in config
