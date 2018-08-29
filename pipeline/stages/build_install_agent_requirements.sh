a9a022a8a984d597f8381f1d236df18950e6dd14
Kurt Waechter <kurt.waechter@blackboard.com>
2018-08-29 02:19:00 -0400

    Adding yes option back

--- pipeline/stages/build_install_agent_requirements.sh
+++ pipeline/stages/build_install_agent_requirements.sh
@@ -7,7 +7,7 @@ wget https://github.com/tsu-denim/ffmpeg-portable/releases/download/v0.1-alpha/f
 wget https://github.com/tsu-denim/chromium-full-gtk2-lambda/releases/download/63.2.33/chrome-63.0.3239.84.tar.gz --quiet &
 
 sudo -H apt-get install software-properties-common -qq
-sudo -H add-apt-repository ppa:deadsnakes/ppa
+sudo -H add-apt-repository --yes ppa:deadsnakes/ppa -qq
 sudo -H apt-get update -qq
 sudo -H apt-get install python3.6 -qq
 curl -sS https://bootstrap.pypa.io/get-pip.py | sudo -H python3.6
