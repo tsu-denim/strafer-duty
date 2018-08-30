3fc0de2f4d94347de74e8e6e77b641aa33757373
tsu-denim <tsu-denim@users.noreply.github.com>
2018-08-29 20:07:44 -0400

    Updating command for travis, fixing typo

--- pipeline/stages/run_integration_jasmine-allure.sh
+++ pipeline/stages/run_integration_jasmine-allure.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-python3 -m pytest --junitxml=junit_results/e2e-allure.xml \
---report_path='allure-report/data/test-cases/*' \
+sudo -H python3.6 -m pytest --junitxml="junit_results/e2e-allure.xml" \
+--report_path="allure-report/data/test-cases/*" \
 pipeline/integration_tests/functional_tests/integ_tests_allure.py
 set +ex
