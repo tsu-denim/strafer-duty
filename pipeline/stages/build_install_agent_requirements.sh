2236a2f0dbb9da9896a8b51c2646888bccddb7c0
Kurt Waechter <tsu-denim@users.noreply.github.com>
2018-11-02 17:00:53 -0400

    Update build_install_agent_requirements.sh
    Bump chrome version to 68

--- pipeline/stages/build_install_agent_requirements.sh
+++ pipeline/stages/build_install_agent_requirements.sh
@@ -5,7 +5,7 @@ echo "Forking downloads of repo release binaries from github..."
 wget https://github.com/tsu-denim/xvfb-portable/releases/download/v1.0/xvfb.tar.gz --quiet &
 wget https://github.com/tsu-denim/node-portable/releases/download/node-v6.11.4-linux-x64/node.tar.gz --quiet &
 wget https://github.com/tsu-denim/ffmpeg-portable/releases/download/v0.1-alpha/ffmpeg.tar.gz --quiet &
-wget https://github.com/tsu-denim/chromium-full-gtk2-lambda/releases/download/63.2.33/chrome-63.0.3239.84.tar.gz --quiet &
+wget https://github.com/tsu-denim/chromium-full-gtk2-lambda/releases/download/68.2.41/chrome-68.0.3440.75.tar.gz --quiet &
 
 echo "Forking complete, waiting for operations to complete..."
 wait
